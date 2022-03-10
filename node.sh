09b7fbeb6416b6a01a2cda3f5595cae9bd4e9808
Ajay Kc <99326419+jtestkc@users.noreply.github.com>
2022-03-10 10:48:53 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install git
-git clone https://github.com/jtestkc/bash-scriptu.git
-chmod a+x node.sh
-bash node.sh
 curl -sL https://deb.nodesource.com/setup_14.x | bash -
 sudo apt-get install nodejs -y
 sudo npm install npm@latest -g -y
